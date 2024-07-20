785e529924a4597c9b588ba6ef9e757ee7730064
Arthur <arthur>
2024-07-20 03:16:29 +0300

    hp steam balance

--- kubejs/server_scripts/gtca/alt_fuel/high_pressure_steam.js
+++ kubejs/server_scripts/gtca/alt_fuel/high_pressure_steam.js
@@ -1,16 +1,16 @@
 ServerEvents.recipes(event => {
 /// STEAM TURBINE ///
         event.recipes.gtceu.steam_turbine('hp_steam')
-            .inputFluids('gtceu:high_pressure_steam 100')
+            .inputFluids('gtceu:high_pressure_steam 64')
             .outputFluids('gtceu:distilled_water 2')
-            .duration(30)
+            .duration(25)
             .EUt(-32)
 
 /// RECIPE ///
         event.recipes.gtceu.steam_pressurizer('hp_steam')
-        .inputFluids('gtceu:steam 1000')
-        .outputFluids('gtceu:high_pressure_steam 100')
+        .inputFluids('gtceu:steam 6400')
+        .outputFluids('gtceu:high_pressure_steam 640')
         .duration(5)
-        .EUt(32)
+        .EUt(240)
 }
 )
