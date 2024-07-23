5d6a2189f6babf71db9e254d26ec4df44ab42d9b
Arthur <arthur>
2024-07-24 02:54:32 +0300

    chem gen recepies rework

--- kubejs/server_scripts/gtca/alt_fuel/chem_gen_energy_recipes.js
+++ kubejs/server_scripts/gtca/alt_fuel/chem_gen_energy_recipes.js
@@ -1,72 +1,46 @@
 ServerEvents.recipes(event => {
+    const greg = event.recipes.gtceu;
+    const std = [
+        ['dsa','gtceu:diluted_sulfuric_acid', 20, 20, -32],
+        ['aa','gtceu:acetic_acid', 8, 30, -32],
+        ['dha','gtceu:diluted_hydrochloric_acid', 15, 25, -32],
+        ['sa','gtceu:sulfuric_acid', 10, 20, -90],
+        ['na','gtceu:nitric_acid', 10, 25, -96],
+        ['ha','gtceu:hydrochloric_acid', 8, 25, -98],
+        ['merc','gtceu:mercury', 12, 40, -76],
+        ['pa','gtceu:phosphoric_acid', 9, 20, -82],
+        ['hfa','gtceu:hydrofluoric_acid', 8, 40, -90],
+        ['pta','gtceu:phthalic_acid', 6, 40, -72],
+        ['fa','gtceu:formic_acid', 6, 20, -96],
+        ['hba','gcyr:hydrobromic_acid', 4, 40, -108],
+        ['dinp','gtceu:diisononyl_phthalate', 4, 70, -120],
+    ];
 
-    event.recipes.gtceu.chem_gen('DSA_gen')
-    .inputFluids('gtceu:diluted_sulfuric_acid 20')
-    .duration(20)
-    .EUt(-32)
+    std.forEach((type) => {
+        if (type[0].includes('dinp')) {
+            greg.chem_gen(type[0])
+            .inputFluids(type[1] + ' ' + type[2].toString())
+            .itemInputs('2x gtceu:small_coal_dust')
+            .duration(type[3])
+            .EUt(type[4])  
+        } else {
+            greg.chem_gen(type[0])
+            .inputFluids(type[1] + ' ' + type[2].toString())
+            .duration(type[3])
+            .EUt(type[4])
+        }
 
-    event.recipes.gtceu.chem_gen('AS_gen')
-    .inputFluids('gtceu:acetic_acid 8')
-    .duration(30)
-    .EUt(-32)
-
-    event.recipes.gtceu.chem_gen('DHL_gen')
-    .inputFluids('gtceu:diluted_hydrochloric_acid 15')
-    .duration(25)
-    .EUt(-32)
-
-    event.recipes.gtceu.chem_gen('SA_gen')
-    .inputFluids('gtceu:sulfuric_acid 10')
-    .duration(20)
-    .EUt(-90)
-
-
-    event.recipes.gtceu.chem_gen('NA_gen')
-    .inputFluids('gtceu:nitric_acid 10')
-    .duration(25)
-    .EUt(-96)
-
-
-    event.recipes.gtceu.chem_gen('HCL_gen')
-    .inputFluids('gtceu:hydrochloric_acid 8')
-    .duration(25)
-    .EUt(-98)
-
-    event.recipes.gtceu.chem_gen('merc_gen')
-    .inputFluids('gtceu:mercury 12')
-    .duration(40)
-    .EUt(-76)
-
-
-    event.recipes.gtceu.chem_gen('PA_gen')
-    .inputFluids('gtceu:phosphoric_acid 9')
-    .duration(20)
-    .EUt(-82)
-
-    event.recipes.gtceu.chem_gen('HF_acid')
-    .inputFluids('gtceu:hydrofluoric_acid 8')
-    .duration(40)
-    .EUt(-90)
-
-    event.recipes.gtceu.chem_gen('DINP')
-    .inputFluids('gtceu:diisononyl_phthalate 4')
-    .itemInputs('2x gtceu:small_coal_dust')
-    .duration(70)
-    .EUt(-120)
-
-    event.recipes.gtceu.chem_gen('PHT_acid')
-    .inputFluids('gtceu:phthalic_acid 6')
-    .duration(40)
-    .EUt(-72)
-
-    event.recipes.gtceu.chem_gen('form_gen')
-    .inputFluids('gtceu:formic_acid 6')
-    .duration(20)
-    .EUt(-96)
-
-    event.recipes.gtceu.chem_gen('ggcey_hbro_gen')
-    .inputFluids('gcyr:hydrobromic_acid 4')
-    .duration(40)
-    .EUt(-108)
-
+        if (type[0].includes('dinp')) {
+            greg.chem_gen(type[0] + "_boosted")
+            .itemInputs('2x gtceu:small_coal_dust')
+            .inputFluids(type[1] + ' ' + ( 2 * type[2]).toString(), 'gtceu:oxygen 10')
+            .duration(type[3])
+            .EUt(2 * type[4])
+        } else {
+            greg.chem_gen(type[0] + "_boosted")
+            .inputFluids(type[1] + ' ' + ( 2 * type[2]).toString(), 'gtceu:oxygen 10')
+            .duration(type[3])
+            .EUt(2 * type[4])
+        }
+    });
 })
